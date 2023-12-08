1e0fba316832a72df78242db0f74e0eb4df6ceb7
Soskretkov <oskrserg@gmail.com>
2023-12-09 02:36:33 +0300

    на др комп

--- src/components/header/components/logo.rs
+++ src/components/header/components/logo.rs
@@ -1,15 +1,18 @@
 use leptos::*;
 use crate::components::Icon;
+use leptos_router::*;
 
 #[component]
 pub fn Logo() -> impl IntoView {
     view! {
-        <div class="flex -mt-3">
+        // <div class="flex -mt-3">
+        <A href="/" class="flex -mt-3 no-underline text-current">
             <Icon id="fa-code" class="text-[70px] mr-[10px]}"/>
             <div class="">
                 <div class="text-[48px] font-semibold leading-[48px] mt-4">"Блог"</div>
                 <div class="text-[18px] font-bold">"веб-разработка"</div>
             </div>
-        </div>
+        // </div>
+        </A>
     }
 }
