7f0308f9fbdfb591055c2ca5a2f0a4be05711e24
Soskretkov <oskrserg@gmail.com>
2023-12-09 05:15:54 +0300

    исправил пропавшие иконки

--- src/components/header/components/logo.rs
+++ src/components/header/components/logo.rs
@@ -4,9 +4,9 @@ use leptos_router::*;
 
 #[component]
 pub fn Logo() -> impl IntoView {
-    view! {
+    view! {      
         <A href="/" class="flex -mt-3 no-underline text-current">
-            <Icon id="fa-code" class="text-[70px] mr-[10px]}"/>
+            <Icon id="fa-code" class="text-[70px] mr-[10px]"/>
             <div class="">
                 <div class="text-[48px] font-semibold leading-[48px] mt-4">"Блог"</div>
                 <div class="text-[18px] font-bold">"веб-разработка"</div>
--- src/components/icon.rs
+++ src/components/icon.rs
@@ -1,7 +1,7 @@
 use leptos::*;
 
 #[component]
-pub fn Icon(id: &'static str, #[prop(optional)] class: Option<&'static str>) -> impl IntoView {
+pub fn Icon(id: &'static str, #[prop(optional)] class: &'static str) -> impl IntoView {
     view! {
         <div class=class>
             <i class=format!("fa {}", id) aria-hidden="true"></i>
