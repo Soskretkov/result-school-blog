a5173f2bb9ea4ca04a374754fa94da0ad3987adc
Soskretkov <oskrserg@gmail.com>
2023-12-21 13:43:21 +0300

    users теперь html таблица

--- src/pages/users.rs
+++ src/pages/users.rs
@@ -1,9 +1,9 @@
 use super::components::H2;
 use leptos::*;
-mod table_body_row;
+mod body_row;
 mod table_row;
 use crate::types::outer_api::{Role, User};
-use table_body_row::TableBodyRow;
+use body_row::BodyRow;
 use table_row::TableRow;
 
 #[component]
@@ -19,26 +19,30 @@ pub fn Users() -> impl IntoView {
     view! {
         <div class="flex items-center flex-col w-[570px] mx-auto">
             <H2>"Пользователи"</H2>
-            <div class="flex items-center">
-                <div class="w-[172px] px-2.5">"Логин"</div>
-                <div class="w-[213px] px-2.5">"Дата регистрации"</div>
-                <div class="w-auto px-2.5">"Роль"</div>
-                <div>"удалить"</div>
-            </div>
-            <TableRow>
-                {users
-                    .with(
-                        |data| data
-                            .into_iter()
-                            .map(|user| {
-                                view!{
-                                    <TableBodyRow user={user}/>
-                                }
-                            })
-                            .collect_view()
-                    )
-                }
-            </TableRow>
+            <table>
+                <thead>
+                    <tr class="flex items-center">
+                        <th class="w-[172px] px-2.5">"Логин"</th>
+                        <th class="w-[213px] px-2.5">"Дата регистрации"</th>
+                        <th class="w-[150px] px-2.5">"Роль"</th>
+                        <th class="w-auto"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users
+                        .with(
+                            |data| data
+                                .into_iter()
+                                .map(|user| {
+                                    view!{
+                                        <BodyRow user={user}/>
+                                    }
+                                })
+                                .collect_view()
+                        )
+                    }
+                </tbody>
+            </table>
         </div>
     }
 }
--- src/pages/users/body_row.rs
+++ src/pages/users/body_row.rs
@@ -4,19 +4,21 @@ use leptos::*;
 
 // лайфтайм нужен потому как возвращаемыйimpl IntoView зависит от ссылки
 #[component]
-pub fn TableBodyRow<'a>(user: &'a User) -> impl IntoView {
+pub fn BodyRow<'a>(user: &'a User) -> impl IntoView {
     // удалить в бд, запросить пользователей, обновить ресурс или сигнал
     let on_click = |_: ev::MouseEvent| !unimplemented!();
     view! {
-        <div class="flex">
-            <div class="w-[172px] px-2.5">{user.login.clone()}</div>
-            <div class="w-[213px] px-2.5">{user.registered_at.clone()}</div>
-            <div class="flex w-auto px-2.5">
+        <tr class="flex">
+            <td class="w-[172px] px-2.5">{user.login.clone()}</td>
+            <td class="w-[213px] px-2.5">{user.registered_at.clone()}</td>
+            <td class="flex w-[150px] px-2.5">
                 <RoleSelect user_role_id={user.role_id}></RoleSelect>
                 <Icon on:click=on_click id="fa-floppy-o" class="cursor-pointer text-[24px] ml-2.5"/>
-            </div>
-            <Icon on:click=on_click id="fa-trash-o" class="cursor-pointer text-[24px] ml-2.5"/>
-        </div>
+            </td>
+            <td class="w-auto">
+                <Icon on:click=on_click id="fa-trash-o" class="cursor-pointer text-[24px] ml-2.5"/>
+            </td>
+        </tr>
     }
 }
 
