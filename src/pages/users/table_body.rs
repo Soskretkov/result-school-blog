28a767ff59558df76d042238795297ebd1b54b4d
Soskretkov <oskrserg@gmail.com>
2023-12-22 20:10:57 +0300

    For html

--- src/pages/users/table_body.rs
+++ src/pages/users/table_body.rs
@@ -5,50 +5,72 @@ use leptos::*;
 
 #[component]
 pub fn TableBody() -> impl IntoView {
-    // удалить в бд, запросить пользователей, обновить ресурс или сигнал
-    let on_click = |_: ev::MouseEvent| unimplemented!();
-
     let users_res = create_resource(|| (), |_| async { bff_utils::all_users::<User>().await });
     let roles_res = create_resource(|| (), |_| async { bff_utils::all_roles::<Role>().await });
 
+    // Ресурсы также предоставляют refetch()метод, который позволяет вручную перезагрузить данные.
+
     view! {
         <tbody>
-            {move || match users_res.get() {
-                None => ().into_view(),
-                Some(vec) => {
-                    vec
-                        .into_iter()
-                        .map(|user| {
-                            view! {
-                                <tr class="flex">
-                                    <td class="w-[172px] px-2.5">{user.login.clone()}</td>
-                                    <td class="w-[213px] px-2.5">{user.registered_at.clone()}</td>
-                                    <td class="flex w-[150px] px-2.5">
-                                        <RoleSelect
-                                            roles={roles_res}
-                                            user_role_id={user.role_id}
-                                        ></RoleSelect>
-                                        <Icon
-                                            on:click=on_click
-                                            id="fa-floppy-o"
-                                            class="cursor-pointer text-[24px] ml-2.5"
-                                        />
-                                    </td>
-                                    <td class="w-auto">
-                                        <Icon
-                                            on:click=on_click
-                                            id="fa-trash-o"
-                                            class="cursor-pointer text-[24px] ml-2.5"
-                                        />
-                                    </td>
-                                </tr>
-                            }
-                        })
-                        .collect_view()
+            {move || {
+                // match users_res.get() {
+                //     None => ().into_view(),
+                //     Some(vec) => {
+                        view! {
+                            <Suspense
+                                fallback=move || view! { <p>"Loading..."</p> }
+                            >
+                                <For
+                                    each=move || users_res.get().unwrap_or(Vec::new())
+                                    key=|user| user.id.clone()
+                                    children=move |user| {
+                                        view! {
+                                            <BodyRow
+                                                user={user}
+                                                roles={roles_res}
+                                            />
+                                        }
+                                    }
+                                />
+                            </Suspense>
+                        }
+                    }
                 }
-            }}
+            // }}
         </tbody>
 
+    }
+}
+
+#[component]
+// pub fn BodyRow(user: User) -> impl IntoView {
+pub fn BodyRow(user: User, roles: Resource<(), Vec<Role>>) -> impl IntoView {
+    // удалить в бд, запросить пользователей, обновить ресурс или сигнал
+    let on_click = |_: ev::MouseEvent| unimplemented!();
+
+    view! {
+        <tr class="flex">
+        <td class="w-[172px] px-2.5">{user.login.clone()}</td>
+        <td class="w-[213px] px-2.5">{user.registered_at.clone()}</td>
+        <td class="flex w-[150px] px-2.5">
+            <RoleSelect
+                roles={roles}
+                user_role_id={user.role_id}
+            ></RoleSelect>
+            <Icon
+                on:click=on_click
+                id="fa-floppy-o"
+                class="cursor-pointer text-[24px] ml-2.5"
+            />
+        </td>
+        <td class="w-auto">
+            <Icon
+                on:click=on_click
+                id="fa-trash-o"
+                class="cursor-pointer text-[24px] ml-2.5"
+            />
+        </td>
+    </tr>
     }
 }
 
--- src/types/outer_api.rs
+++ src/types/outer_api.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
 pub struct User {
     pub id: String,
     pub login: String,
