73ce3044b4ac2f39680e6af72e3d4c89f8fd4690
Soskretkov <oskrserg@gmail.com>
2024-03-10 20:35:23 +0300

    4. Статьи и комментарии: 8. Страница Статья. Верстка комментариев

--- src/pages/post/comments.rs
+++ src/pages/post/comments.rs
@@ -10,12 +10,15 @@ pub fn Comments(post_id: String, comments: Vec<Comment>) -> impl IntoView {
     let on_new_comment_add = {
         let add_action = create_action(move |&()| {
             let post_id_clon = post_id.clone();
-            let new_content = comment_node_ref.get().unwrap().value();
+            let comment_node_ref = comment_node_ref.get().unwrap();
 
             async move {
-                server::add_comment(post_id_clon, new_content)
+                server::add_comment(post_id_clon, comment_node_ref.value())
                     .await
-                    .map(|cmnt| set_comments_signal.update(|vec| vec.push(cmnt)))
+                    .map(|cmnt| {
+                        set_comments_signal.update(|vec| vec.push(cmnt));
+                        comment_node_ref.set_value("")
+                    })
             }
         });
 
@@ -26,14 +29,14 @@ pub fn Comments(post_id: String, comments: Vec<Comment>) -> impl IntoView {
     };
 
     view! {
-        <div class="w-[580px] my-0 mx-auto"> // comments class
+        <div class="w-[580px] mt-0 mx-auto"> // CommentsConainer
             // форма добавления нового комментария
-            <form on:submit = on_new_comment_add class="flex w-full mt-[20px] items-start"> // new-comment class
+            <form on:submit = on_new_comment_add class="flex w-full mt-[20px] items-start"> // new-comment
                 <textarea
                     name="comment"
                     value={""}
-                    placeholder="Комментарий..."
-                    class="text-[18px] pl-[2px] w-full h-[120px] border border-black rounded-sm outline-none"
+                    placeholder="Комментарий . . . "
+                    class="w-[550px] h-[120px] text-[18px] pl-[2px]  border border-gray-500 rounded-sm outline-none"
                     node_ref = comment_node_ref
                 />
                 <button
@@ -45,8 +48,7 @@ pub fn Comments(post_id: String, comments: Vec<Comment>) -> impl IntoView {
                     />
                 </button>
             </form>
-
-
+            
             <For // все существующие комментарии
                 each=move || comments_signal.get()
                 key=|cmnt| cmnt.id.clone()
@@ -65,21 +67,21 @@ pub fn Comment(comment: Comment) -> impl IntoView {
     let created_at = comment.created_at.format("%Y-%m-%d %H:%M").to_string();
 
     view! {
-        <div class="flex">
-            <div class="">
+        <div class="flex mt-[10px]"> // ComentContainer
+            <div class="w-[550px] py-[5px] px-[10px] border border-black rounded-sm"> // comment class
                 <div class="flex justify-between"> // information-panel class
                     <div class="flex"> // author class
-                        <Icon id="fa-user-circle-o" class="text-[18px] mr-[7px]"/>
+                        <Icon id="fa-user-circle-o" class="text-[18px] mr-[10px]"/>
                         {comment.user_name_snapshot}
                     </div>
-                    <div class="flex">
-                        <Icon id="fa-calendar-o" class="top-[-1px] text-[18px] mr-[7px]"/>
-                        <div class = "">{created_at}</div>
+                    <div class="flex"> // published_at class
+                        <Icon id="fa-calendar-o" class="text-[18px] mr-[10px] top-[-1px]"/>
+                        {created_at}
                     </div>
                 </div>
-                <div class="">{comment.content}</div>
+                <div class="">{comment.content}</div> // comment-text class
             </div>
-            <Icon id="fa-trash-o" class="text-[21px]"/>
+            <Icon id="fa-trash-o" class="text-[21px] ml-[10px] cursor-pointer"/>
         </div>
     }
 }
